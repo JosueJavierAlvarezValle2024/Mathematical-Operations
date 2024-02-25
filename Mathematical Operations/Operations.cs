cebd8053b307b354ff647e5a70263a01e2a44f5b
Jjav2 <Jjav2@HPCOMPUTER18>
2024-02-24 20:15:16 -0600

    Agregar archivos de proyecto.

---
+++ Mathematical Operations/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematical_Operations
+{
+    public class Operations
+    {
+        public int Sum(int x , int y) // As this method is public, everyone has access to it.
+        {
+            return x + y;
+        }
+        protected int Subtract (int m , int n) // As this is protected, only derived classes have access.
+        {
+            return m - n;
+        }
+
+        internal int Multiplication (int e, int f) // As this method is internal, only classes contained in the same assembly have access.
+        {
+            return e * f;
+        }
+
+        private double Divison (int c, int d) // As this method is private, only the class that defines it has access to it.
+        {
+            if (d != 0) // An if is used in case you cannot divide by zero.
+                return c / d;
+            else
+            {
+                Console.WriteLine("In case it is 0, it cannot be divided.");
+                return double.NaN; // NaN is used if the result of an operation is not defined.
+            }
+        }
+    }
+}
---
+++ Mathematical Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿// Operations mathematical (+)(-)(*)(/)
+using Mathematical_Operations;
+
+Operations operations = new Operations();
+
+int sum = operations.Sum(6, 8); // Here the public method is executed in the sum operation
+Console.WriteLine("The sum is: "+sum);
+
+Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+//int subtract = operations.Subtract(5, 3); // Here an error occurs because the subtract method is protected.
+//Console.WriteLine("Subtraction is: "+subtract);
+
+Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+int multiplication = operations.Multiplication(8, 10);
+Console.WriteLine("Multiplication is: "+multiplication);
+
+Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+//double divition = operations.Division(8, 2); // As the divide method is private, it cannot be accessed from outside the class.
+//Console.WriteLine("Division is :"+divition);
+
+Console.WriteLine("---------------------------------------------------------------------------------------------");
+Console.ReadLine();
